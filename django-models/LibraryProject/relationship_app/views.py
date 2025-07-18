8f8e0f70d85ad4cb59b1464e582bd555a8736121
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-18 16:26:57 +0300

    lazymode

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Book, Library
+from .models import Library
 
 # Function-Based View (strict match)
 def list_books(request):
