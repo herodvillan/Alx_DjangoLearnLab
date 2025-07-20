e06c4e626423078b5afce8f5dd82b1db7952fe32
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-20 05:06:44 +0300

    k

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -30,15 +30,15 @@ def is_member(user):
 # Views
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'admin_view.html')
+    return render(request, 'relation_app/admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'librarian_view.html')
+    return render(request, 'relation_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, 'member_view.html')
+    return render(request, 'relation_app/member_view.html')
 
 
 # Function-Based View (strict match)
