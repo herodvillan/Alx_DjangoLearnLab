689d0cbdb83e9abf01145f1aea0779f65d8bda0d
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-20 04:38:24 +0300

    k

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # function-based
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # class-based
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import rende, redirect
 from django.views.generic.detail import DetailView
 from .models import Book, Library
-from .models import Library
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import views as auth_views
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form}
 
 # Function-Based View (strict match)
 def list_books(request):
