5d0628a31c7171159182d597e17dd189b690c092
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-20 04:40:49 +0300

    k

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from .models import Book, Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import views as auth_views
-
+from django.contrib.auth import login
 
 def register(request):
     if request.method == 'POST':
