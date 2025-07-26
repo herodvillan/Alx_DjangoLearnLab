631b00c3be5f904315c141af6a4e7fa7f4941271
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-26 22:37:29 +0300

    l

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,7 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea, required=False)
+
 #lets try this
