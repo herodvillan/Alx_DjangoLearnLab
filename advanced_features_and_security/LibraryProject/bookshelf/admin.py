a863a69eae3201ead96fffaae8c30a6801739360
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-26 22:02:51 +0300

    wrong code

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,15 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Extra Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields |= {'is_staff': True, 'is_superuser': True}
-        return self.create_user(username, email, password, **extra_fields)
-
-# Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
