af836ea6bec158ac8d1e6d4248197e8aa310eed0
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-26 21:57:38 +0300

    k

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields |= {'is_staff': True, 'is_superuser': True}
+        return self.create_user(username, email, password, **extra_fields)
 
 # Register your models here.
