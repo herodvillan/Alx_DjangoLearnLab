94373c9c1053cbd097ec787785d3d4daa8e0bf30
Yusuf <yusuf.d.kemal@gmail.com>
2025-07-29 14:29:20 +0300

    lk

---
+++ api_project/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from .views import BookListCreateAPIView
+
+urlpatterns = [
+    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
+]
+
