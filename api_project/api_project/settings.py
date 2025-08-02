15c8a46d4204c67ffde0db39ee11b81ff0128bd1
Yusuf <yusuf.d.kemal@gmail.com>
2025-08-03 02:19:38 +0300

    j

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -101,6 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
