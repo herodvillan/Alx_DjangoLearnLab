eab44e247eca79018b5940fc81285934fcc0e09a
Yusuf <yusuf.d.kemal@gmail.com>
2025-08-10 15:41:42 +0300

    hjkl

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 class Author(models.Model)
     name = CharField
-class Book(models.Model)
-    title = CharField
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.CharField(max_length=255)
 # Create your models here.
