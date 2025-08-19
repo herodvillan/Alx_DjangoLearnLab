6fda52e501400e50155c5b6a5c41fa846198c9b5
Yusuf <yusuf.d.kemal@gmail.com>
2025-08-19 23:32:42 +0300

    l

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,12 @@
-from django.contrib.auth.models import User, author
+from django.contrib.auth.models import User
 from django.db import models
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
 
 class post(models.Model):
     title: models.CharField(max_length=200)
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrin.staticfiles',
     'blog',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -78,8 +77,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER':
-        'PORT':
     }
 }
 
