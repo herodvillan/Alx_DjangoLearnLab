8efa937a8859be5f061561df734fab37ec30ceee
Yusuf <yusuf.d.kemal@gmail.com>
2025-08-22 13:55:11 +0300

    k

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,8 +2,7 @@ from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
+User = get_user_model()  # ensures we use the custom User model
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -14,7 +13,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password", "bio", "profile_picture", "token"]
 
     def create(self, validated_data):
-        # 1. Create user with hashed password
+        # Create user using get_user_model().objects.create_user
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email", ""),
@@ -22,13 +21,12 @@ class RegisterSerializer(serializers.ModelSerializer):
             bio=validated_data.get("bio", ""),
             profile_picture=validated_data.get("profile_picture", None),
         )
-        # 2. Create a token for the new user
+        # Create a token for the new user
         token = Token.objects.create(user=user)
         user.token = token.key  # temporary for serializer output
         return user
 
     def to_representation(self, instance):
-        # Return token in the response
         data = super().to_representation(instance)
         data["token"] = Token.objects.get(user=instance).key
         return data
