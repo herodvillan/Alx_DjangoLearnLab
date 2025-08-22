138327840626a4383cc0c1d21ba1914a1de92a2f
Yusuf <yusuf.d.kemal@gmail.com>
2025-08-22 14:29:26 +0300

    o

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+# --------------------------
+# Post ViewSet
+# --------------------------
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=["post"])
+    def like(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+        if user in post.likes.all():
+            post.likes.remove(user)
+            return Response({"status": "unliked"})
+        else:
+            post.likes.add(user)
+            return Response({"status": "liked"})
+
+
+# --------------------------
+# Comment ViewSet
+# --------------------------
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by("created_at")
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
