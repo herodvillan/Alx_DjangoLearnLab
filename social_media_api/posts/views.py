b509490da34ffb49b3e05f62df6d9c9ab127f3ec
Yusuf <yusuf.d.kemal@gmail.com>
2025-08-24 10:54:40 +0300

    k

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,39 +1,34 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from django.shortcuts import get_object_or_404
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+
 
 # --------------------------
 # Post ViewSet
 # --------------------------
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by("-created_at")
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        """
+        Show posts from the current user + users they follow
+        """
+        user = self.request.user
+        following_users = user.following.all()  # ✅ get all followed users
+        return Post.objects.filter(author__in=following_users).order_by("-created_at") | Post.objects.filter(author=user).order_by("-created_at")
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    @action(detail=True, methods=["post"])
-    def like(self, request, pk=None):
-        post = self.get_object()
-        user = request.user
-        if user in post.likes.all():
-            post.likes.remove(user)
-            return Response({"status": "unliked"})
-        else:
-            post.likes.add(user)
-            return Response({"status": "liked"})
 
 
 # --------------------------
 # Comment ViewSet
 # --------------------------
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by("created_at")
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
